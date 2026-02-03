a58a3212d1df3fb338d4d3fe595f48f77b358cef
Ziang Li <ziangli@umich.edu>
2026-02-02 20:26:58 -0800

    Revert "Add nvfp4."
    This reverts commit 67602144efea2da451edbd46778fe2c4aeab9bca.

--- miles/backends/megatron_utils/megatron_to_hf/processors/__init__.py
+++ miles/backends/megatron_utils/megatron_to_hf/processors/__init__.py
@@ -2,14 +2,12 @@ from .padding_remover import remove_padding
 from .quantizer_compressed_tensors import quantize_params_compressed_tensors
 from .quantizer_fp8 import quantize_params_fp8
 from .quantizer_mxfp8 import quantize_params_mxfp8
-from .quantizer_nvfp4 import quantize_params_nvfp4
 
 __all__ = [
     "remove_padding",
     "quantize_param",
     "quantize_params_fp8",
     "quantize_params_mxfp8",
-    "quantize_params_nvfp4",
     "quantize_params_compressed_tensors",
 ]
 
@@ -21,8 +19,6 @@ def quantize_params(args, megatron_name, converted_named_params, quantization_co
         return quantize_params_fp8(args, megatron_name, converted_named_params, quantization_config)
     elif quantization_config["quant_method"] == "mxfp8":
         return quantize_params_mxfp8(args, megatron_name, converted_named_params, quantization_config)
-    elif quantization_config.get("quant_algo") == "NVFP4" or quantization_config.get("quant_method") == "nvfp4":
-        return quantize_params_nvfp4(args, megatron_name, converted_named_params, quantization_config)
     elif quantization_config["quant_method"] == "compressed-tensors":
         # only int4 at the moment.
         return quantize_params_compressed_tensors(converted_named_params, quantization_config)
